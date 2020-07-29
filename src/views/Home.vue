5cf290c47a6997cc2dfc6856e65592522fa9d432
Stefan Borović <33036655+sborovic@users.noreply.github.com>
2020-07-30 00:37:50 +0200

    Create project and install Storefront UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SfButton/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import { SfButton } from '@storefront-ui/vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    SfButton,
   },
 };
 </script>
