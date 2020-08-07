4c7bf1d07b1538755b23e679be837424218f4bc3
Stefan Borović <33036655+sborovic@users.noreply.github.com>
2020-08-07 23:23:33 +0200

    Import CoreuiVue library and styles globally and add an example component (AdminNavbars)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import CoreuiVue from '@coreui/vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import '@coreui/coreui/scss/coreui.scss';
 import '@storefront-ui/vue/styles.scss';
 
 Vue.config.productionTip = false;
+Vue.use(CoreuiVue);
 
 new Vue({
   router,
